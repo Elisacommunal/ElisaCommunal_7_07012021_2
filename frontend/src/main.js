4f1edf31a19d3492b4665579ae568a6b761fe6cc
Elisa Communal <elisa.communal.simplon@gmail.com>
2021-08-24 14:20:07 +0200

    add assets

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '../style/style.css'
     
 createApp(App).use(store).use(router).mount('#app')
